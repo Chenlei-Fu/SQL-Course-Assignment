87869976f7f63ca022042cb4902d41015f527bb7
Charlie Fu <charliefu@CharliedeMacBook-Pro.local>
2020-04-17 11:26:44 -0500

    First commit

---
+++ Assignment_01.sql
@@ -0,0 +1,35 @@
+/*what customer_ids are eligibal for platinum status*/
+SELECT customer_id, COUNT(*)
+FROM payment
+GROUP BY customer_id
+HAVING  COUNT(*) > 40;
+
+/*The customer_ids who have spent more than 100 
+in payment transactions with our staff_id meber 2*/
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id = '2'
+GROUP BY customer_id
+Having SUM(amount) > 100;
+
+/*Assignment Task1*/
+/*1. Return the customer IDs of customers who have 
+spent at least $110 with the staff member who has an ID of 2.*/
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id = '2'
+GROUP BY customer_id
+HAVING SUM(amount) > 110;
+
+/*2. How many films begin with the letter J?*/
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'J%';
+
+/*3. What customer has the highest customer ID number 
+whose name starts with an 'E' and has an address ID lower than 500?*/
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1
